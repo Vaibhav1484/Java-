f1a019e537b5eb32236d4ce30efac9880fee9b9c
Vaibhav1484 <101709176+Vaibhav1484@users.noreply.github.com>
2024-03-03 15:53:40 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class array {
+//     private static void swap(int[] arr,int[] brr){
+//         int temp=arr[0];
+//         arr[0]=brr[0];
+//         brr[0]=temp;
+//     }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // int[] arr= new int[7];
+        
+        // System.out.println(arr);   // to print address
+        // System.out.println(arr.length);    // to print the length of the array
+//giving inputs on our own in the array
+        // arr[0]=1;
+        // arr[1]=2;
+        // arr[2]=3;
+        // arr[3]=4;
+        // arr[4]=5;
+        // arr[5]=6;
+        // arr[6]=17;
+        // arr[-1]=85;
+        // System.out.println(arr[5]); //printing particular element
+//taking input arr        
+        // System.out.print("Enter: ");
+
+        // for(int i=0;i<arr.length;i++){
+        //     arr[i]=sc.nextInt();
+        // }
+//to print arr as a string
+        // System.out.print(Arrays.toString(arr));  
+////to print arr
+        // for(int i=0;i<arr.length;i++){
+        // }
+        //     System.out.print(arr[i]+" ");
+
+//swappigg  array
+
+//     int[] arr={1,2,3,4,5};
+//     int[] brr={6,7,8,9,0};
+//     System.out.println(Arrays.toString(arr));
+//     System.out.println(Arrays.toString(brr));  
+//     System.out.println(arr[0] + " " + brr[0]);
+
+//     swap(arr, brr);
+//     System.out.println(arr[0] + " " + brr[0]);
+//     System.out.println(Arrays.toString(arr)); 
+//     System.out.println(Arrays.toString(brr)); 
+
+// array rotation
+        // System.out.print("enter array size: ");
+        // int n=sc.nextInt();
+
+        // int[] arr= new int[n];
+
+        // System.out.print("Enter array: ");
+        // for(int i=0;i<n;i++){
+        //         arr[i] = sc.nextInt();
+        // }
+
+        // System.out.print("Enter times to be rotated: ");
+        // int k = sc.nextInt();
+ 
+        // int val = arr[arr.length - 1];
+        // int j = 0;
+        // while( j < k ){
+        //         for(int i = arr.length -2; i>=0; i--){
+        //                 arr[i+1]=arr[i];
+        //         }
+        //         arr[0]=val;
+        //         j++;
+        // }
+        // System.out.print("Array after rotation: ");
+
+        // System.out.print(Arrays.toString(arr));
+
+        
+    }
+}
