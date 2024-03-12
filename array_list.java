a21590334dab484f96c7155b2f0afd92148823e2
Vaibhav1484 <101709176+Vaibhav1484@users.noreply.github.com>
2024-03-12 12:34:16 +0530

    Add files via upload

---
+++ array_list.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class array_list {
+    public static void main(String[] args){
+        ArrayList<Integer> al = new ArrayList<>();
+        // ArrayList<Integer> al = new ArrayList(); // integer here specifies the datatype already
+        
+        al.add(1);
+        // al.add(10.0);
+        // al.add(true);
+        al.add(10);
+        // al.add("abc");
+        // al.add("c");       
+        // al.add("c");
+       
+        System.out.println(al);
+    }
+}
+
